0c119d7fd15ff60ac44813f1bdd67ebf762f49d5
Deivith Maciel <deivithfagundes@gmail.com>
2026-06-24 18:34:31 -0300

    feat: add first types and typed functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
+import { useState } from "react"
+
 function App() {
+  type Usuario = {
+    id: number
+    nome: string
+    ativo: boolean
+  }
+
+  const [usuarios, setUsuarios] = useState<Usuario[]>([
+    {
+      id: 1,
+      nome: 'Ana',
+      ativo: true
+    }
+  ])
+
   return (
     <div>
       <h1>React + Typescript</h1>
