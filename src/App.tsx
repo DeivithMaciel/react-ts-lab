de84b274ca9b8b519e80b3aa9e389760650e1c79
Deivith Maciel <deivithfagundes@gmail.com>
2026-06-27 16:57:04 -0300

    feat: migrate Card component to TypeScript with typed props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 
 import type { Usuario } from "./types/Usuario"
+import Card from "./components/Card"
 
 function App() {
   const [usuarios, setUsuarios] = useState<Usuario[]>([
@@ -8,12 +9,63 @@ function App() {
       id: 1,
       nome: 'Ana',
       ativo: true
+    },
+    {
+      id: 2,
+      nome: 'João',
+      ativo: false
     }
   ])
+  const [nome, setNome] = useState('')
+  const [busca, setBusca] = useState('')
+  const [darkMode, setDarkMode] = useState(false)
+
+  function removeUser(id: number) {
+    const novaLista = usuarios.filter((user) => user.id !== id)
+    setUsuarios(novaLista)
+  }
+
+  function activeToggle(id: number) {
+    const novaLista = usuarios.map((user) => {
+      if (user.id === id) {
+        return {
+          ...user,
+          ativo: !user.ativo
+        }
+      }
+      return user
+    })
+    setUsuarios(novaLista)
+  }
+
+  function editUser (id: number, novoNome: string) {
+    const novaLista = usuarios.map((user) => {
+      if (user.id === id) {
+        return {
+          ...user,
+          nome: novoNome
+        }
+      }
+      return user
+    })
+    setUsuarios(novaLista)
+  }
 
   return (
     <div>
       <h1>React + Typescript</h1>
+      <ul>
+        {usuarios.map((user) => (
+          <Card
+        key={user.id}
+        user={user}
+        editUser={editUser}
+        activeToggle={activeToggle}
+        removeUser={removeUser}
+        />
+        ))}
+      </ul>
+      <button onClick={(() => setDarkMode(!darkMode))}>{darkMode ? 'Light Mode' : 'Dark Mode'}</button>
     </div>
   )
 }
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,20 +2,24 @@ import type { Usuario } from "../../types/Usuario"
 
 type CardProps = {
     user: Usuario
-    removeUser (id: number) => void
+    removeUser: (id: number) => void
     editUser: (id: number, novoNome: string) => void
     activeToggle: (id: number) => void
+}
 
 export const Card = ({
     user,
-    removerUser,
+    removeUser,
     editUser,
     activeToggle
-}:CardProps) => {
-    }
+}: CardProps) => {
     return (
         <li>
-            card
+            <h3>{user.nome}</h3>
+            <p>{user.ativo ? "🟢 Ativo" : "🔴 Inativo"}</p>
+            <button onClick={(() => activeToggle(user.id))}>{user.ativo ? 'Desativar' : 'Ativar'}</button>
+            <button onClick={(() => editUser(user.id, user.nome))}>Editar usuario</button>
+            <button onClick={(() => removeUser(user.id))}>Remover usuario</button>
         </li>
     )
 }
