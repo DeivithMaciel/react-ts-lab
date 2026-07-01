372e060452313392d9cdc79de655f0199bf204f2
Deivith Maciel <deivithfagundes@gmail.com>
2026-07-01 19:30:52 -0300

    feat: add user statistics dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,9 @@ function App() {
   const [busca, setBusca] = useState('')
   const [darkMode, setDarkMode] = useState(false)
 
+  const usuariosAtivos = usuarios.filter((user) => user.ativo)
+  const usuariosInativos = usuarios.filter((user) => !user.ativo)
+
   function addUser(nome: string) {
     if (!nome.trim()) return
     const usuarioExiste = usuarios.find((user) => user.nome.toLowerCase() === nome.toLowerCase())
@@ -67,12 +70,17 @@ function App() {
     setUsuarios(novaLista)
   }
 
-
     const nomesFiltrados = usuarios.filter((user) => user.nome.toLowerCase().includes(busca.toLowerCase()))
 
   return (
     <div>
       <h1>React + Typescript</h1>
+      <div>
+        <span>👥Total de usuarios:{usuarios.length}</span>
+        <span>🟢Ativos:{usuariosAtivos.length}</span>
+        <span>🔴 Inativos:{usuariosInativos.length}</span>
+        <span>🔍 Resultado da busca:{nomesFiltrados.length}</span>
+      </div>
       <div>
         <input value={nome} onChange={(e) => setNome(e.target.value)} />
         <button onClick={() => addUser(nome)}>Adicionar</button>
@@ -90,6 +98,7 @@ function App() {
         ))}
       </ul>
       <button onClick={(() => setDarkMode(!darkMode))}>{darkMode ? 'Light Mode' : 'Dark Mode'}</button>
+      <button></button>
     </div>
   )
 }
