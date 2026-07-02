e07f1bd276b5754b07ee930adcad35abc6fe912a
Deivith Maciel <deivithfagundes@gmail.com>
2026-07-02 17:50:53 -0300

    chore: add TypeScript learning notes

---
+++ src/studies/generics.ts
@@ -0,0 +1,10 @@
+type Usuario = {
+  id: number
+  nome: string
+}
+
+const usuarios: Usuario[] = []
+
+const promise: Promise<Usuario> = new Promise(() => {})
+
+const numeros: Array<number> = [1, 2, 3]
