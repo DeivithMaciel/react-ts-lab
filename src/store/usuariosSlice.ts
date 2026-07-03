207608df080e7eee4e507135db4f7fae6406a474
Deivith Maciel <deivithfagundes@gmail.com>
2026-07-03 18:45:14 -0300

    chore: initialize Redux Toolkit structure

---
+++ src/store/usuariosSlice.ts
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const usuariosSlice = createSlice({
+    name: 'usuarios',
+    initialState: [],
+    reducers: {
+    }
+})
+
+export default usuariosSlice.reducer
