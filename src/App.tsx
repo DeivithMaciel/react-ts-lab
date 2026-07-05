c77a38ce7e45a074ecc10245c7add5d6639f73e6
Deivith Maciel <deivithfagundes@gmail.com>
2026-07-05 18:47:20 -0300

    refactor: organize application into pages with React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,20 @@
-import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-
-import type { RootState, AppDispatch } from "./store/store"
-import { addUserAPI, fetchUsuarios } from "./features/Usuarios/usuariosSlice"
-
-import Card from "./components/Card"
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Carrinho from "./pages/Carrinho";
+import NotFound from "./pages/NotFound";
+import Produtos from "./pages/Produtos";
+import Usuarios from "./pages/Usuarios";
 
 function App() {
-  const dispatch = useDispatch<AppDispatch>();
-  const usuarios = useSelector((state: RootState) => state.usuarios)
-
-  const [nome, setNome] = useState('')
-  const [busca, setBusca] = useState('')
-  const [darkMode, setDarkMode] = useState(false)
-
-  const usuariosAtivos = usuarios.filter((user) => user.ativo)
-  const usuariosInativos = usuarios.filter((user) => !user.ativo)
-  const nomesFiltrados = usuarios.filter((user) => user.nome.toLowerCase().includes(busca.toLowerCase()))
-
-  useEffect(() => {
-    dispatch(fetchUsuarios())
-  })
-
   return (
-    <div>
-      <h1>React + Typescript</h1>
-      <div>
-        <span>👥Total de usuarios:{usuarios.length}</span>
-        <span>🟢Ativos:{usuariosAtivos.length}</span>
-        <span>🔴 Inativos:{usuariosInativos.length}</span>
-        <span>🔍 Resultado da busca:{nomesFiltrados.length}</span>
-      </div>
-      <div>
-        <input value={nome} onChange={(e) => setNome(e.target.value)} />
-        <button onClick={() => dispatch(addUserAPI(nome))}>Adicionar</button>
-        <input onChange={((e) => setBusca(e.target.value))} value={busca} placeholder="buscar usuario" />
-      </div>
-      <ul>
-        {nomesFiltrados.map((user) => (
-          <Card
-        key={user.id}
-        user={user}
-        />
-        ))}
-      </ul>
-      <button onClick={(() => setDarkMode(!darkMode))}>{darkMode ? 'Light Mode' : 'Dark Mode'}</button>
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/carrinho" element={<Carrinho />} />
+      <Route path="*" element={<NotFound />} />
+      <Route path="/produtos" element={<Produtos />} />
+      <Route path="/usuarios" element={<Usuarios />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 
 import { store } from './store/store.ts'
 
@@ -8,8 +9,10 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
+    <BrowserRouter>
+      <Provider store={store}>
       <App />
     </Provider>
+    </BrowserRouter>
   </StrictMode>,
 )
