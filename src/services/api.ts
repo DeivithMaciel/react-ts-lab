26d09463a626e659a2f098685ff4de9d09781a94
Deivith Maciel <deivithfagundes@gmail.com>
2026-07-11 11:15:52 -0300

    refactor: replace fetch with axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,2 +1,8 @@
+import axios from "axios"
+
 export const API_URL = "http://localhost:3001/usuarios"
 export const API_PRODUTOS = "http://localhost:3001/produtos"
+
+export const api = axios.create({
+    baseURL: "http://localhost:3001"
+})
--- src/services/produtos.ts
+++ src/services/produtos.ts
@@ -1,24 +1,12 @@
 import type { Produto } from "../types/Produto";
-import { API_PRODUTOS } from "./api";
+import { api } from "./api";
 
 export async function getProdutos() {
-    const res = await fetch(API_PRODUTOS)
-    if (!res.ok) {
-        throw new Error('Erro ao buscar produtos')
-    }
-    return (await res.json()) as Produto[]
+    const { data } = await api.get<Produto[]>('/produtos')
+    return data
 }
 
 export async function createProduto(produto: Produto) {
-    const res = await fetch(API_PRODUTOS , {
-        'method': 'POST',
-        headers: {
-            'Content-type':'application/json'
-        },
-        body: JSON.stringify(produto)
-    })
-    if (!res.ok) {
-        throw new Error ('Erro ao criar produto')
-    }
-    return res.json()
+    const {data} = await api.post<Produto>('/produtos', produto)
+    return data
 }
